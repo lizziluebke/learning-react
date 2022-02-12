c5e31186d3e904479f999414ceb149a6764b79a6
lizziluebke <77250113+lizziluebke@users.noreply.github.com>
2022-02-11 20:38:13 -0600

    created my first react component named greeting
    put it in a file by itself

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './Greeting.js'; 
+
+
+
 
 const App = () => {
   return (
     <div>
-    <h1>Hello React</h1>
-    <h1>Hello Larry</h1>
+      <h1>Hello React</h1>
+      <h1>Hello Larry</h1>
+       <Greeting />
     </div>
   );
 }; 
