9a5628e81721d82c6f5bbeb2b2247155fd6b5d0b
lizziluebke <77250113+lizziluebke@users.noreply.github.com>
2022-02-15 19:24:46 -0600

    showing css in js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,23 @@ import Greeting from './Greeting.js';
 
 
 const App = () => {
+
+  const styles = {
+    container: {
+      padding: 30
+      , backgroundColor: new Date().getSeconds() % 2 === 0 ? 'lightblue' : 'pink'
+    }
+    , heading: {
+      textTransform: 'uppercase'
+    }
+  };
+  
+  
   return (
-    <div>
+    <div
+      style={styles.container}
+    >
+      
       <h1>Hello React</h1>
       
        <Greeting favoritenumber={1} name='Larry'
