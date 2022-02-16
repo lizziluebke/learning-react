89f80268b28a86ccd8ea79af1d876fd4676dc9cc
lizziluebke <77250113+lizziluebke@users.noreply.github.com>
2022-02-15 18:51:42 -0600

    destructing props

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,21 +1,25 @@
 import { Literal } from "jade/lib/nodes";
 
-const Greeting = (props) => {
+const Greeting = ({
+  name 
+  , favoriteColor
+  , favoritenumber
+}) => {
 
-    console.log(props); 
+    //console.log(props); 
 
 
     return (
         <>
-      <h2>Hello {props.name}</h2>
-      <h3>{props.name}'s favorite number is {props.favoritenumber}</h3>
-      <h3>{props.name}'s favorite number + 10 is {props.favoritenumber + 10}</h3>
+      <h2>Hello {name}</h2>
+      <h3>{name}'s favorite number is {favoritenumber}</h3>
+      <h3>{name}'s favorite number + 10 is {favoritenumber + 10}</h3>
       <ul>
-          {props.favoriteColor.map(x => <li>{x}</li>)}
+          {favoriteColor.map(x => <li>{x}</li>)}
       </ul>
       </>
     ); 
-  
+
   
   }; 
 
